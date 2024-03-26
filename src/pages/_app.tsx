d08c007009d7235c5e341387add94ce326c873a4
ulises-jeremias <ulisescf.24@gmail.com>
2024-03-26 03:03:46 -0300

    Add environment variable for Sanity API read token and update siteSettings schema

--- src/pages/_app.tsx
+++
@@ -1,7 +0,0 @@
-import { AppProps } from 'next/app';
-
-export interface SharedPageProps {}
-
-export default function App({ Component, pageProps }: AppProps<SharedPageProps>) {
-  return <Component {...pageProps} />;
-}
