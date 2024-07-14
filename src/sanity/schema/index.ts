31c99197fd1d103d8448cfbf34d9fb5b224c86d2
ulises-jeremias <ulisescf.24@gmail.com>
2024-07-14 17:34:22 -0300

    feat: Update content rendering

--- src/sanity/schema/index.ts
+++ src/sanity/schema/index.ts
@@ -1,42 +1,19 @@
 import { SchemaTypeDefinition } from 'sanity';
 import { navigationMenu } from './documents/navigationMenu';
-import {
-  heroSection,
-  headerSection,
-  faqSection,
-  featureSection,
-  featureSectionLeftImage,
-  featureSectionThreeCol,
-  logoCloudsSection,
-  testimonialSection,
-  ctaSection,
-  imageSection,
-  subheaderSection,
-} from './singletons/sections';
+import { imageSection } from './singletons/sections';
 import { footer } from './singletons/footer';
 import { page } from './documents/page';
 import { siteSettings } from './documents/siteSettings';
 import { navigationItem } from './singletons/navigationItem';
 import { colors } from './singletons/colors';
-import { customRichTextBlock, colorPicker, featureItem } from './types';
+import { customRichTextBlock, colorPicker } from './types';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
     colors,
     navigationItem,
     navigationMenu,
-    heroSection,
-    headerSection,
-    faqSection,
-    featureItem,
-    featureSection,
-    featureSectionLeftImage,
-    featureSectionThreeCol,
-    logoCloudsSection,
-    testimonialSection,
-    ctaSection,
     imageSection,
-    subheaderSection,
     footer,
     page,
     siteSettings,
