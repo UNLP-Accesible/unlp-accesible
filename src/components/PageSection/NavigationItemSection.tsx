898c3502956b0a55ad74ba84c26fafd7eb5d68c6
ulises-jeremias <ulisescf.24@gmail.com>
2024-09-15 16:25:39 -0300

    Refactor logo display in NavigationItemSection component

--- src/components/PageSection/NavigationItemSection.tsx
+++ src/components/PageSection/NavigationItemSection.tsx
@@ -17,8 +17,12 @@ const NavigationItemSection: FC<NavigationItemSectionProps> = ({ text, logo, slu
       style={{ color, backgroundColor }}
       className="flex flex-row items-center p-5 rounded-lg transition duration-300 ease-in-out"
     >
-      {logo && <Image src={logo} alt="Logo" width={65} height={65} />}
-      <div className="flex-1">
+      {logo && (
+        <div className="flex justify-center items-center" style={{ height: '55px', width: '55px' }}>
+          <Image src={logo} alt="Logo" width={55} height={55} style={{ objectFit: 'contain' }} />
+        </div>
+      )}
+      <div className="flex-1 pl-3">
         <p className="text-lg text-center font-semibold">{text}</p>
       </div>
     </a>
